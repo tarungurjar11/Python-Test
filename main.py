59892ed9192cead2072752f22676fe27045796fc
tarungurjar11 <tarunmuchhal11@gmail.com>
2025-11-21 18:07:25 +0530

    test new commit

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ app = FastAPI()
 def home():
     return {"message": "FastAPI is live, baby!"}
 
+# test comment to trigger redeployment  
+@app.get("/status")
+def status():
+    return {"status": "OK"}
